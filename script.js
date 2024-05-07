d522627aa7133e8aae5c7835839617cc4603dfce
javiermaxi <131915527+javiermaxi@users.noreply.github.com>
2024-05-07 18:38:45 -0300

    cambios en codigo

--- script.js
+++ script.js
@@ -41,35 +41,11 @@ $(document).ready(function(){
         loop: false
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Developer", "and", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  
 
     // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+   
+    
 });
 
 // Dark Mode
@@ -80,3 +56,5 @@ btnSwitch.addEventListener('click',() => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 })
+
+
